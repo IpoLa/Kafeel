6e6fc2a45d26b95f1cb7cff5167c1a1cfe1e39ea
IpoLa <akramchat@hotmail.com>
2020-04-19 19:49:09 +0100

    HomaVid19

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,26 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .form import SignUpForm
 # Create your views here.
-def login(request):
-    # title = "Hello guest !"
-    # if request.user.is_authenticated():
-        # title = "Hello %s" %(request.user)
-    form = SignUpForm()
-    context = {
-        # "template_title" : title,
+# def login(request):
+#     # title = "Hello guest !"
+#     if request.user.is_authenticated():
+#         title = "Hello %s" %(request.user)
+#     form = SignUpForm()
+#     context = {
+#         "template_title" : title,
+#         "form" : form
+    # }
+    # return render(request, "login.html", context)
+def home(request):
+    if request.user.is_authenticated():
+            form = SignUpForm()
+            context = {
+            "form" : form
+            }
+            return render(request, "home.html",context)
+    if not request.user.is_authenticated():
+        form = SignUpForm()
+        context = {
         "form" : form
-    }
-    return render(request, "login.html", context)
+        }
+        return render(request, "login.html", context)
---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+"use strict";
+
+$('.slider-active').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+})(jQuery);
